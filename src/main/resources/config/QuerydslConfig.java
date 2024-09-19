3e649916ff193e937a62459b53c0768993d72ad9
Jisue_Jung <wjdwltn0909@gmail.com>
2024-09-19 13:24:19 +0900

    240919 1323 템플릿 부분수정 및 관리자페이지 추가

---
+++ src/main/resources/config/QuerydslConfig.java
@@ -0,0 +1,10 @@
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
