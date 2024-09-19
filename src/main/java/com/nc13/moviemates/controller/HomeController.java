4256f741b70ef307a9c4e2d71690c44e902bbf57
Jisue_Jung <wjdwltn0909@gmail.com>
2024-09-19 10:07:16 +0900

    240919 1006 - template 합본

--- src/main/java/com/nc13/moviemates/controller/HomeController.java
+++ src/main/java/com/nc13/moviemates/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.nc13.moviemates.controller;
 
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Controller
 public class HomeController {
@@ -9,4 +19,10 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/single")
+    public String toSingle() {
+        return "single";
+    }
+
 }
