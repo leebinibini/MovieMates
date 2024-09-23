fad95ff9007b7510e5c2f9a0d46e809fa7864b65
Jisue_Jung <wjdwltn0909@gmail.com>
2024-09-23 21:53:10 +0900

    240923 2152 image storage에 저장하려고 하는 중

---
+++ src/main/java/com/nc13/moviemates/service/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.nc13.moviemates.service.impl;
+
+import com.nc13.moviemates.model.entity.UserEntity;
+import com.nc13.moviemates.model.repository.UserRepository;
+import com.nc13.moviemates.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+
+    @Override
+    public List<?> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<UserEntity> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Boolean save(UserEntity user) {
+        UserEntity ent = repository.save(user);
+        Long id = ent.getId();
+        return existsById(id);
+    }
+
+    @Override
+    public Boolean deleteById(Long id) {
+        repository.deleteById(id);
+        return !existsById(id);
+    }
+
+    @Override
+    public Long count() {
+        return repository.count();
+    }
+
+    @Override
+    public Boolean existsById(Long id) {
+        return repository.existsById(id);
+    }
+}
