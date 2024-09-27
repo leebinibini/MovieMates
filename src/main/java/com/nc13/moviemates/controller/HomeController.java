db158b31fa6f7003b819508b4b94e6ff283bb0e7
bini <qpdjvn0601@naver.com>
2024-09-27 21:05:55 +0900

    rendering homepage

--- src/main/java/com/nc13/moviemates/controller/HomeController.java
+++ src/main/java/com/nc13/moviemates/controller/HomeController.java
@@ -1,17 +1,29 @@
 package com.nc13.moviemates.controller;
 
+import com.nc13.moviemates.entity.MovieEntity;
+import com.nc13.moviemates.service.MovieService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+
+    private final MovieService movieService;
+
+    //홈페이지 화면 가져오기
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+            List<MovieEntity> movie = movieService.findAll();
+            model.addAttribute("movies", movie);
+
         return "index";
     }
 
