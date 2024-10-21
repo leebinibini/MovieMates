a6a04f255d78972299a66e8318b26d4a47289a80
Jisue_Jung <wjdwltn0909@gmail.com>
2024-10-21 16:27:48 +0900

    1006 1627 - 중간 커밋 및 푸시

--- src/main/java/com/nc13/moviemates/serviceImpl/ImageServiceImpl.java
+++ src/main/java/com/nc13/moviemates/serviceImpl/ImageServiceImpl.java
@@ -5,8 +5,8 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.nc13.moviemates.component.model.ImageModel;
-import com.nc13.moviemates.component.model.MovieModel;
 import com.nc13.moviemates.config.BucketConfig;
+import com.nc13.moviemates.component.model.MovieModel;;
 import com.nc13.moviemates.entity.ImageEntity;
 import com.nc13.moviemates.entity.MovieEntity;
 import com.nc13.moviemates.repository.ImageRepository;
@@ -27,8 +27,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
     private final ImageRepository repository;
-    private final MovieService movieService;
-    private final MovieRepository movieRepository;
+    private final MovieServiceImpl movieService;
     private final AmazonS3 amazonS3;
     String uploadPath = "uploads/movies";
     // 버킷경로 가져오기
@@ -44,11 +43,9 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public Boolean uploadFiles(long movieId, List<MultipartFile> multipartFiles) {
-        System.out.println("이미지 서비스 업로드파일 함수 진입!!!");
         // 파일 저장 공간을 리스트 형태로 만들겠다!
         List<ImageModel> s3files = new ArrayList<>();
 
-        // Find movie entity
         MovieEntity movieEntity = movieService.findEntityById(movieId);
         if (movieEntity == null) {
             throw new IllegalArgumentException("Invalid movieId: " + movieId);
@@ -74,17 +71,18 @@ public class ImageServiceImpl implements ImageService {
             try (InputStream inputStream = multipartFile.getInputStream()) {
                 String keyName = uploadPath + "/" + storedFileName;
 
-                amazonS3.putObject(new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata));
-                System.out.println("keyName = " + keyName);
+                amazonS3.putObject(
+                        new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata));
+                System.out.println("keyName = 은!!!!" + keyName);
 
-                // Generate the upload URL
+                // 결국 최종 URL : uploadURL
                 uploadURL = "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + keyName;
                 System.out.println("uploadURL = " + uploadURL);
             } catch (IOException e) {
                 throw new RuntimeException("파일 업로드 실패: " + e.getMessage());
             }
 
-            // Convert ImageModel to ImageEntity and save
+            // Model로 받아와서 Entity로 변환!
             ImageModel imageModel = ImageModel.builder()
                     .originalFilename(originalFilename)
                     .storedFileName(storedFileName)
