a658022d97a03c0cf897c1a6db31c34dc8a95678
Jisue_Jung <wjdwltn0909@gmail.com>
2024-11-02 12:17:07 +0900

    20241102 1215 - aws key 분리 후

--- src/main/java/com/nc13/moviemates/MoviematesApplication.java
+++ src/main/java/com/nc13/moviemates/MoviematesApplication.java
@@ -1,6 +1,7 @@
 package com.nc13.moviemates;
 
 import com.nc13.moviemates.service.MovieService;
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,12 @@ public class MoviematesApplication {
     private MovieService movieService;
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load(); // .env 파일 로드
+        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
+        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
         SpringApplication.run(MoviematesApplication.class, args);
+
     }
 
     // CommandLineRunner로 크롤링 작업 자동 실행
